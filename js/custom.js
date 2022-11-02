1547db59a58469bcb3f301b0f8b936d628d6b47f
AussieDev81 <aussiedev81@gmail.com>
2022-11-02 22:03:47 +1000

    load remaining resources to be inline with the parent domain

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// NIVO LIGHTBOX
+$("#portfolio a").nivoLightbox({
+	effect: "fadeScale",
+});
+
+// HIDE MOBILE MENU AFTER CLICKING ON A LINK
+// $(".navbar-collapse a").click(function () {
+// 	$(".navbar-collapse").collapse("hide");
+// });
